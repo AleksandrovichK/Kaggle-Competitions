86f729f2aa8f8a606d23c75ad4c7113c2db507ae
AleksandrovichK <killertom1997@gmail.com>
2019-09-05 03:31:12 +0300

    Added huge feature engineering work

--- code/HousePricesAdvancedRegressionTechniques.py
+++ code/HousePricesAdvancedRegressionTechniques.py
@@ -9,15 +9,22 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+from scipy.stats import skew
 from xgboost import XGBRegressor
 
 from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, KFold
+
+
+#   DISCLAIMER
+# Guys, everything that you see in the kernel you can freely take, copy, modify and create your own, amazing solutions!
+# Do not forget upvote if the kernel was useful for you. Anyway tt will be further developed.
 
 # TODO
-# 1. Add CV
-# 2. Check whether is this correct to always imput 0. Maybe it takes sense to impute with 'mean' or another.
 # 3. Check performance with LabelEncoder as well. And then check LabelEncoder and OH together.
+# 4. Add Lasso, Ridge and some other estimators, test them together and compile them into the ensemble with coefficients
+# 5. log1p haven't improved results, investigate why
+# 6. Create new features or steal them somewhere
 print("Imports have been set")
 
 # Disabling warnings
@@ -26,181 +33,208 @@ if not sys.warnoptions:
 
 # Reading the training/val data and the test data
 X = pd.read_csv('../input/house-prices-advanced-regression-techniques/train.csv', index_col='Id')
-X_test_full = pd.read_csv('../input/house-prices-advanced-regression-techniques/test.csv', index_col='Id')
-
-# Showing general info
-print("Data:")
-print(X.head())
+X_test = pd.read_csv('../input/house-prices-advanced-regression-techniques/test.csv', index_col='Id')
 
 # Rows before:
 rows_before = X.shape[0]
-# Removing rows with missing/empty target
 X.dropna(axis=0, subset=['SalePrice'], inplace=True)
-# Rows after:
 rows_after = X.shape[0]
 print("\nRows containing NaN in SalePrice were dropped: " + str(rows_before - rows_after))
 
-# Separating target from predictors
-y = X.SalePrice
-X.drop(['SalePrice'], axis=1, inplace=True)
+# Logarithming target variable in order to make distribution better
+X['SalePrice'] = np.log1p(X['SalePrice'])
 
-# Spltting X and y to train and validation sets
-X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
-print("\nShape of X_train: " + str(X_train_full.shape) + ", shape of y_train: " + str(y_train.shape))
-print("Shape of X_valid_full: " + str(X_valid_full.shape) + ", shape of y_valid: " + str(y_valid.shape))
+y = X['SalePrice'].reset_index(drop=True)
+train_features = X.drop(['SalePrice'], axis=1)
 
-# Select categorical columns
-categoric_columns = [cname for cname in X_train_full.columns if X_train_full[cname].dtype == "object"]
-print("\nColumns whice are categoric (suitable for OneHot encoding): " + str(len(categoric_columns)) + " out of " + str(X_train_full.shape[1]))
-print(categoric_columns)
+# concatenate the train and the test set as features for tranformation to avoid mismatch
+features = pd.concat([train_features, X_test]).reset_index(drop=True)
+print('\nFeatures size:', features.shape)
 
-# Select numeric columns
-numeric_columns = [cname for cname in X_train_full.columns if X_train_full[cname].dtype in ['int64', 'float64']]
-print("\nColumns which are numeric (no encoding required): " + str(len(numeric_columns)) + " out of " + str(X_train_full.shape[1]))
-print(numeric_columns)
-
-nan_count_table = (X_train_full.isnull().sum())
+nan_count_table = (features.isnull().sum())
 nan_count_table = nan_count_table[nan_count_table > 0].sort_values(ascending=False)
 print("\nColums containig NaN: ")
 print(nan_count_table)
 
 columns_containig_nan = nan_count_table.index.to_list()
 print("\nWhat values they contain: ")
-print(X_train_full[columns_containig_nan])
+print(features[columns_containig_nan])
 
-# imputing numeric columns (for a while - all numeric to zero)
-for column in numeric_columns:
-    X_train_full[column].fillna(value=0, inplace=True)
-    X_valid_full[column].fillna(value=0, inplace=True)
-    X_test_full[column].fillna(value=0, inplace=True)
+# ==============================================
+# FEATURE ENGINEERING
+# Let's perform feature engineering for each column checking what it is and filling gaps / nans
+# ==============================================
+for column in columns_containig_nan:
 
-# imputng categoric columns
-imputer = SimpleImputer(strategy='constant', fill_value='missing_value')
-imputed_X_train = pd.DataFrame(imputer.fit_transform(X_train_full), columns=X_train_full.columns).astype(X_train_full.dtypes.to_dict())
-imputed_X_valid = pd.DataFrame(imputer.transform(X_valid_full), columns=X_valid_full.columns).astype(X_valid_full.dtypes.to_dict())
-imputed_X_test = pd.DataFrame(imputer.transform(X_test_full), columns=X_test_full.columns).astype(X_test_full.dtypes.to_dict())
+    # populating with 0
+    if column in ['GarageYrBlt', 'GarageArea', 'GarageCars', 'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', '1stFlrSF', '2ndFlrSF', 'LowQualFinSF',
+                  'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd', 'TotalBsmtSF',
+                  'Fireplaces', 'WoodDeckSF', 'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'MiscVal', 'MoSold', 'YrSold',
+                  'OverallQual', 'OverallCond', 'YearBuilt', 'YearRemodAdd', 'MasVnrArea']:
+        features[column] = features[column].fillna(0)
 
-# Sorting in order to observe results
-# sorted_before_imput = X_train_full.sort_values('MSSubClass', ascending=True)
-# sorted_after_imput = imputed_X_train.sort_values('MSSubClass', ascending=True)
+    # populate with 'None'
+    if column in ['GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', "PoolQC", 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1',
+                  'BsmtFinType2', 'Neighborhood', 'BldgType', 'HouseStyle', 'MasVnrType', 'FireplaceQu', 'Fence', 'MiscFeature']:
+        features[column] = features[column].fillna('None')
 
-print("\nLet's check whether shape changed or not for X_train. \nBefore imput: " + str(X_train_full.shape) + "\nAfter imput: " + str(imputed_X_train.shape))
+    # populate with most frequent value for cateforic
+    if column in ['Street', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope', 'Condition1', 'Condition2', 'RoofStyle',
+                  'Electrical', 'Functional', 'KitchenQual', 'Exterior1st', 'Exterior2nd', 'SaleType', 'RoofMatl', 'ExterQual', 'ExterCond',
+                  'Foundation', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 'KitchenQual', 'PavedDrive', 'SaleType', 'SaleCondition']:
+        features[column] = features[column].fillna(features[column].mode()[0])
 
-nan_count_train_table = (imputed_X_train.isnull().sum())
+# MSSubClass: Numeric feature. Identifies the type of dwelling involved in the sale.
+#     20  1-STORY 1946 & NEWER ALL STYLES
+#     30  1-STORY 1945 & OLDER
+#     40  1-STORY W/FINISHED ATTIC ALL AGES
+#     45  1-1/2 STORY - UNFINISHED ALL AGES
+#     50  1-1/2 STORY FINISHED ALL AGES
+#     60  2-STORY 1946 & NEWER
+#     70  2-STORY 1945 & OLDER
+#     75  2-1/2 STORY ALL AGES
+#     80  SPLIT OR MULTI-LEVEL
+#     85  SPLIT FOYER
+#     90  DUPLEX - ALL STYLES AND AGES
+#    120  1-STORY PUD (Planned Unit Development) - 1946 & NEWER
+#    150  1-1/2 STORY PUD - ALL AGES
+#    160  2-STORY PUD - 1946 & NEWER
+#    180  PUD - MULTILEVEL - INCL SPLIT LEV/FOYER
+#    190  2 FAMILY CONVERSION - ALL STYLES AND AGES
+
+# Stored as number so converted to string.
+features['MSSubClass'] = features['MSSubClass'].apply(str)
+features["MSSubClass"] = features["MSSubClass"].fillna("Unknown")
+# MSZoning: Identifies the general zoning classification of the sale.
+#    A    Agriculture
+#    C    Commercial
+#    FV   Floating Village Residential
+#    I    Industrial
+#    RH   Residential High Density
+#    RL   Residential Low Density
+#    RP   Residential Low Density Park
+#    RM   Residential Medium Density
+
+# 'RL' is by far the most common value. So we can fill in missing values with 'RL'
+features['MSZoning'] = features.groupby('MSSubClass')['MSZoning'].transform(lambda x: x.fillna(x.mode()[0]))
+# LotFrontage: Linear feet of street connected to property
+# Groupped by neighborhood and filled in missing value by the median LotFrontage of all the neighborhood
+# TODO may be 0 would perform better than median?
+features['LotFrontage'] = features.groupby('Neighborhood')['LotFrontage'].transform(lambda x: x.fillna(x.median()))
+# LotArea: Lot size in square feet.
+# Stored as string so converted to int.
+features['LotArea'] = features['LotArea'].astype(np.int64)
+# Alley: Type of alley access to property
+#    Grvl Gravel
+#    Pave Paved
+#    NA   No alley access
+
+# So. If 'Street' made of 'Pave', so it would be reasonable to assume that 'Alley' might be 'Pave' as well.
+features['Alley'] = features['Alley'].fillna('Pave')
+# MasVnrArea: Masonry veneer area in square feet
+# Stored as string so converted to int.
+features['MasVnrArea'] = features['MasVnrArea'].astype(np.int64)
+
+
+# ==============================
+#      ADDING NEW FEATURES
+# ==============================
+features['YrBltAndRemod'] = features['YearBuilt']+features['YearRemodAdd']
+features['TotalSF'] = features['TotalBsmtSF'] + features['1stFlrSF'] + features['2ndFlrSF']
+
+features['Total_sqr_footage'] = (features['BsmtFinSF1'] + features['BsmtFinSF2'] +
+                                 features['1stFlrSF'] + features['2ndFlrSF'])
+
+features['Total_Bathrooms'] = (features['FullBath'] + (0.5 * features['HalfBath']) +
+                               features['BsmtFullBath'] + (0.5 * features['BsmtHalfBath']))
+
+features['Total_porch_sf'] = (features['OpenPorchSF'] + features['3SsnPorch'] +
+                              features['EnclosedPorch'] + features['ScreenPorch'] +
+                              features['WoodDeckSF'])
+
+# If area is not 0 so creating new feature looks reasonable
+features['haspool'] = features['PoolArea'].apply(lambda x: 1 if x > 0 else 0)
+features['has2ndfloor'] = features['2ndFlrSF'].apply(lambda x: 1 if x > 0 else 0)
+features['hasgarage'] = features['GarageArea'].apply(lambda x: 1 if x > 0 else 0)
+features['hasbsmt'] = features['TotalBsmtSF'].apply(lambda x: 1 if x > 0 else 0)
+features['hasfireplace'] = features['Fireplaces'].apply(lambda x: 1 if x > 0 else 0)
+
+print('Features size:', features.shape)
+
+nan_count_train_table = (features.isnull().sum())
 nan_count_train_table = nan_count_train_table[nan_count_train_table > 0].sort_values(ascending=False)
-print("\nAre no NaN here now in train: " + str(nan_count_train_table.size == 0))
+print("\nAre no NaN here now: " + str(nan_count_train_table.size == 0))
+# ============================================
+#       FIXING SKEWED VALUES
+# ============================================
+numeric_columns = [cname for cname in features.columns if features[cname].dtype in ['int64', 'float64']]
+print("\nColumns which are numeric: " + str(len(numeric_columns)) + " out of " + str(features.shape[1]))
+print(numeric_columns)
 
+skewness = features[numeric_columns].apply(lambda x: skew(x))
+print(skewness.sort_values(ascending=False))
 
-nan_count_test_table = (imputed_X_test.isnull().sum())
-nan_count_test_table = nan_count_test_table[nan_count_test_table > 0].sort_values(ascending=False)
-print("Are no NaN here now in test: " + str(nan_count_test_table.size == 0))
-# # Apply label encoder
-# label_encoder = LabelEncoder()
-# for col in good_label_cols:
-#     label_X_train[col] = label_encoder.fit_transform(X_train[col])
-#     label_X_valid[col] = label_encoder.transform(X_valid[col])
+skewness = skewness[abs(skewness) > 0.5]
+skew_features = np.log1p(features[skewness.index])
+print("\nSkewed values: " + str(skewness.index))
 
-print("\nBefore OH-encoding X_train shape: " + str(X_train_full.shape) + " and X_valid: " + str(X_valid_full.shape))
+# Kind of One-Hot encoding
+final_features = pd.get_dummies(features).reset_index(drop=True)
 
-# Apply one-hot encoder to each column with categorical data
-# NOTE: FOR EACH OF THEM
-OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
-OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(imputed_X_train[categoric_columns]), index=X_train_full[categoric_columns].index)
-OH_cols_valid = pd.DataFrame(OH_encoder.transform(imputed_X_valid[categoric_columns]), index=X_valid_full[categoric_columns].index)
-OH_cols_test = pd.DataFrame(OH_encoder.transform(imputed_X_test[categoric_columns]), index=X_test_full[categoric_columns].index)
+# Spliting the data back to train(X,y) and test(X_sub)
+X = final_features.iloc[:len(y), :]
+X_test = final_features.iloc[len(X):, :]
+print('Features size for train(X,y) and test(X_test):')
+print('X', X.shape, 'y', y.shape, 'X_test', X_test.shape)
 
-# Remove categorical columns (will replace with one-hot encoding)
-# Removing all categoric columns
-numeric_X_train = X_train_full.drop(categoric_columns, axis=1)
-numeric_X_valid = X_valid_full.drop(categoric_columns, axis=1)
-numeric_X_test = X_test_full.drop(categoric_columns, axis=1)
+# ==============================================
+#                      ML part
+# ==============================================
 
-# Add one-hot encoded columns to numerical features
-X_train = pd.concat([numeric_X_train, OH_cols_train], axis=1)
-X_valid = pd.concat([numeric_X_valid, OH_cols_valid], axis=1)
-X_test = pd.concat([numeric_X_test, OH_cols_test], axis=1)
+# optimal parameters, received from CV
+c_grid = {"n_estimators": [1000],
+          "early_stopping_rounds": [1],
+          "learning_rate": [0.1]}
+xgb_regressor = XGBRegressor(objective='reg:squarederror')
+cross_validation = KFold(n_splits=5, shuffle=True, random_state=2)
 
-print("After OH-encoding X_train shape: " + str(X_train.shape))
-print(", X_valid: " + str(X_valid.shape))
-print(", X_test: " + str(X_test.shape))
-
-# Keep selected columns only WHY? HOW ABOUT OTHERS?
-# my_cols = low_cardinality_cols + numeric_cols
-
-# Who knows what happens here?
-# X_train = X_train_full[my_cols].copy()
-# X_valid = X_valid_full[my_cols].copy()
-# X_test = X_test_full[my_cols].copy()
-
-# # One-hot encode the data (to shorten the code, we use pandas)
-# X_train = pd.get_dummies(X_train)
-# X_valid = pd.get_dummies(X_valid)
-# X_test = pd.get_dummies(X_test)
-
-# X_train, X_valid = X_train.align(X_valid, join='left', axis=1)
-# X_train, X_test = X_train.align(X_test, join='left', axis=1)
-
-# Store for optimal values
-# opt_mae = 999999999
-# opt_n_estimators = 0
-# opt_early_stopping_rounds = 0
-# opt_learning_rate = 0
-
-# hpo_array = []
-
-# # Define the model
-# for n_estimators in [100, 500, 1000]:
-#     for early_stopping_rounds in [1,2,5]:
-#         for learning_rate in [0.01, 0.05, 0.1, 0.5, 1, 2]:
-#             print("\nn_estimators: " + str(n_estimators))
-#             print("early_stopping_rounds: " + str(early_stopping_rounds))
-#             print("learning_rate: " + str(learning_rate))
-#             my_model_2 = XGBRegressor(random_state=0,
-#                                       n_estimators=n_estimators,
-#                                       early_stopping_rounds=early_stopping_rounds,
-#                                       learning_rate=learning_rate,
-#                                       eval_set=[(X_valid, y_valid)],
-#                                       verbose=False)
-#             # Fit the model
-#             my_model_2.fit(X_train, y_train)
-
-#             # Get predictions
-#             predictions_2 = my_model_2.predict(X_valid)
-
-#             # Calculate MAE
-#             mae_2 = mean_absolute_error(predictions_2, y_valid)
-
-#             hpo_array.append({'mae': mae_2, 'n_estimators':n_estimators, 'early_stopping_rounds':early_stopping_rounds, 'learning_rate': learning_rate})
-
-#             if mae_2 < opt_mae:
-#                 opt_mae = mae_2
-#                 opt_n_estimators = n_estimators
-#                 opt_early_stopping_rounds = early_stopping_rounds
-#                 opt_learning_rate = learning_rate
-
-#             # Uncomment to print MAE
-#             print("Mean Absolute Error:" , mae_2)
-
-my_model = Pipeline([
-    ('scale', StandardScaler()),
-    ('reg', XGBRegressor(objective='reg:squarederror',
-                         n_estimators=1000,
-                         early_stopping_rounds=1,
-                         learning_rate=0.1,
-                         eval_set=[(X_valid, y_valid)],
-                         verbose=False))
-])
-
-my_model.fit(X_train, y_train)
-preds_test = my_model.predict(X_test)
-
-mae = mean_absolute_error(my_model.predict(X_valid), y_valid)
-
-# Uncomment to print MAE
-print("Mean Absolute Error:", mae)
+my_model = GridSearchCV(estimator=xgb_regressor,
+                        param_grid=c_grid,
+                        cv=cross_validation)
+my_model.fit(X, y)
 
 # Save test predictions to file
 # output = pd.DataFrame({'Id': X_test.index,
 #                        'SalePrice': preds_test})
+# submission.iloc[:,1] = np.expm1(blend_models(X_test))
 # output.to_csv('submission.csv', index=False)
 # print("Submission file is formed")
+
+
+# FOR NEXT EDIT:
+
+# # Kernel Ridge Regression : made robust to outliers
+# ridge = make_pipeline(RobustScaler(), RidgeCV(alphas=alphas_alt, cv=kfolds))
+#
+# # LASSO Regression : made robust to outliers
+# lasso = make_pipeline(RobustScaler(), LassoCV(max_iter=1e7,
+#                                               alphas=alphas2,random_state=42, cv=kfolds))
+#
+# # Elastic Net Regression : made robust to outliers
+# elasticnet = make_pipeline(RobustScaler(), ElasticNetCV(max_iter=1e7,
+#                                                         alphas=e_alphas, cv=kfolds, l1_ratio=e_l1ratio))
+#
+# # store models, scores and prediction values
+# models = {'Ridge': ridge,
+#           'Lasso': lasso,
+#           'ElasticNet': elasticnet}
+# predictions = {}
+# scores = {}
+#
+# for name, model in models.items():
+#
+#     model.fit(X, y)
+#     predictions[name] = np.expm1(model.predict(X))
+#
+#     score = cv_rmse(model, X=X)
+#     scores[name] = (score.mean(), score.std())
